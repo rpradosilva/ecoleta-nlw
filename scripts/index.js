1d40c5298637ded0bd18b6c2d2ce4d9d2d7740cc
rpradosilva <contato@rprado.design>
2020-06-03 20:17:35 -0300

    Primeira parte da terceira aula

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,110 @@
+function populateUfs() {
+    const ufSelect = document.querySelector("[name=uf]")
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+        .then(res => res.json())
+        .then(states => {
+            for (state of states) {
+                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+            }
+        })
+}
+
+function getCities(event) {
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=ufname]")
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    citySelect.innerHTML = "<option value>Selecione a cidade</option>"
+    citySelect.disabled = true
+
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${event.target.value}/municipios`)
+        .then(res => res.json())
+        .then(states => {
+            for (state of states) {
+                citySelect.innerHTML += `<option value="${state.nome}">${state.nome}</option>`
+            }
+            citySelect.disabled = false
+        })
+}
+
+populateUfs()
+
+document
+    .querySelector("[name=uf]")
+    .addEventListener("change", getCities)
+
+
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+for (item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+let selectedItems = []
+const collectedItems = document.querySelector("[name=items]")
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+    const itemId = itemLi.dataset.id
+
+    itemLi.classList.toggle("selected")
+
+    const alreadySelected = selectedItems.findIndex(
+        item => item == itemId
+    )
+
+    if (alreadySelected >= 0) {
+
+        const filteredItems = selectedItems.filter(
+            item => item != itemId
+        )
+        selectedItems = filteredItems
+
+    } else {
+
+        selectedItems.push(itemId)
+
+    }
+
+    collectedItems.value = selectedItems
+
+}
+
+
+//console.log(alreadySelected)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
 const urlAPI = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/"
 const ufInput = document.querySelector("[name=ufname]")
 
@@ -25,7 +132,7 @@ function consultAPI(url, dataType) {
 
 consultAPI(urlAPI, "uf")
 
-
+*/
 
 
 
